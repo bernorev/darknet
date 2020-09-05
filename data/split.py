1e0d889ba8be294d9070eca96aa2fcc50fe16c49
berno <berno@pop-os.localdomain>
2020-09-05 18:29:46 +0200

    added split py files

---
+++ data/split.py
@@ -0,0 +1,40 @@
+import glob
+import os
+from tkinter import filedialog
+
+#import tkconstants
+#import tkFileDialog
+while True:
+    print("Please select your image directory.")
+    current_dir = filedialog.askdirectory()
+    if current_dir == None or current_dir == "":
+        print("You must select a directory.")
+        continue
+    break
+# Percentage of images to be used for the test set
+percentage_test = 10
+# Create and/or truncate train.txt and test.txt
+file_train = open('train.txt', 'w')
+file_test = open('test.txt', 'w')
+# Populate train.txt and test.txt
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(current_dir + "/" + title + '.jpg' + "\n")
+    else:
+        file_train.write(current_dir + "/" + title + '.jpg' + "\n")
+        counter = counter + 1
+
+counter = 1
+index_test = round(100 / percentage_test)
+for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.png")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+    if counter == index_test:
+        counter = 1
+        file_test.write(current_dir + "/" + title + '.png' + "\n")
+    else:
+        file_train.write(current_dir + "/" + title + '.png' + "\n")
+        counter = counter + 1
